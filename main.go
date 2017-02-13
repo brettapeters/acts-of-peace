c13bd4ea77732c47563cd1ac295e3190ae110d0a
Brett Peters <brettp@Bretts-Mac-mini.local>
2017-02-12 17:52:26 -0700

    Separate template to its own file

--- main.go
+++ main.go
@@ -37,76 +37,24 @@ func root(w http.ResponseWriter, r *http.Request) {
   }
 }
 
-var actsListTemplate = template.Must(template.New("list").Parse(`
-<!DOCTYPE html>
-<html>
-  <head>
-    <link rel="stylesheet" href="/css/styles.css" />
-    <title>Acts of Peace</title>
-  </head>
-  <body>
-    <table>
-      <thead>
-        <tr>
-          <td>Title</td>
-          <td>Description</td>
-          <td>Focus Area</td>
-        </tr>
-      </thead>
-      <tbody>
-      {{range .}}
-        <tr>
-          <td>{{.Title}}</td>
-          <td>{{.Description}}</td>
-          <td>{{.FocusArea}}
-        </tr>
-      {{end}}
-      </tbody>
-    </table>
-    <form action="/submit" method="post">
-      <div>
-        <label for="title">Title</label>
-        <input type="text" name="title" />
-      </div>
-      <div>
-        <label for="description">Description</label>
-        <textarea name="description" rows="6" cols="60"></textarea>
-      </div>
-      <div>
-        <label for="focusArea">Focus Area</label>
-        <select name="focusArea">
-          <option disabled selected value> -- select a focus area -- </option>
-          <option value="Education and Community Development">Education and Community Development</option>
-          <option value="Protecting the Environment">Protecting the Environment</option>
-          <option value="Alleviating Extreme Poverty">Alleviating Extreme Poverty</option>
-          <option value="Global Health and Wellness">Global Health and Wellness</option>
-          <option value="Non-proliferation & Disarmament">Non-proliferation & Disarmament</option>
-          <option value="Human Rights For All">Human Rights For All</option>
-          <option value="Ending Racism & Hate">Ending Racism & Hate</option>
-          <option value="Advancing Women and Children">Advancing Women and Children</option>
-          <option value="Clean Water For Everyone">Clean Water For Everyone</option>
-          <option value="Conflict Resolution">Conflict Resolution</option>
-        </select>
-      </div>
-      <div><input type="submit" value="Submit"></div>
-    </form>
-  </body>
-</html>
-`))
+var actsListTemplate, _ = template.ParseFiles("templates/acts.html")
 
 func submit(w http.ResponseWriter, r *http.Request) {
-  c := appengine.NewContext(r)
-  g := Act{
-    Title: r.FormValue("title"),
-    Description: r.FormValue("description"),
-    FocusArea: r.FormValue("focusArea"),
+  if r.Method == "POST" {
+    c := appengine.NewContext(r)
+    g := Act{
+      Title: r.FormValue("title"),
+      Description: r.FormValue("description"),
+      FocusArea: r.FormValue("focusArea"),
+    }
+
+    key := datastore.NewIncompleteKey(c, "Act", actsOfPeaceKey(c))
+    _, err := datastore.Put(c, key, &g)
+    if err != nil {
+      http.Error(w, err.Error(), http.StatusInternalServerError)
+      return
+    }
   }
 
-  key := datastore.NewIncompleteKey(c, "Act", actsOfPeaceKey(c))
-  _, err := datastore.Put(c, key, &g)
-  if err != nil {
-    http.Error(w, err.Error(), http.StatusInternalServerError)
-    return
-  }
   http.Redirect(w, r, "/", http.StatusFound)
 }
